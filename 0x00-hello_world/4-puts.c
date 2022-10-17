8a539f56711e30bdf4ff79aad087a8b1c64924d7
ndanumungata <ndanumungata44.com>
2022-10-17 02:45:08 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
 
+/**
+  * main - Entry Point
+  *
+  *Return: Always 0 (Success)
+  */
+
+
 int main(void)
-
 {
-	//string initialisation
-	char Mystr[] = "Programming is like building a multilingual puzzle";
-       
-
-	        puts(Mystr); //writing the string to stdout
-
-		    
-
-		    return 0;
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
