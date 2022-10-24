ff8aa1a89050dae16b61336673fcd42b5437c8e9
ndanumungata <ndanumungata44.com>
2022-10-24 02:40:04 -0700

    Print from 0 to 9 except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - prints numbers 0-9 without 2 and 4
+  * Return: Always 0
+  */
+void print_most_numbers(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		if (x != 2 && x != 4)
+			_putchar(x + '0');
+		a++;
+	}
+	_putchar('\n');
+}
