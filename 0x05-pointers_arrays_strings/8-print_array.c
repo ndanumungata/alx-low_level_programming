0d66e12ba5bda17e579e87a7ac764db3605937c7
ndanumungata <ndanumungata44.com>
2022-10-26 19:58:51 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
-  * print_array - prints n elemens of arrays
+  * print_array - prints n elements of an array
   * @a: array
-  * @n: number of values yo be printed
+  * @n: number of values to be printed
   */
 void print_array(int *a, int n)
 {
